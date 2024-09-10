18f2a659f9a6f1e0567f70de329657e272962fb8
RaulSaenzJr <raul.saenz2@gmail.com>
2024-09-10 14:49:34 -0500

    completed problem 1

--- problems/01-is-five.js
+++ problems/01-is-five.js
@@ -3,7 +3,14 @@ Define a function `isFive` that takes in a number parameter. It should return
 a string of 'is five' if the number is equal to 5 and `null` if it is not.
 */
 
-// Your code here 
+// Your code here
+let isFive = function(num) {
+  if (num === 5) {
+    return 'is five';
+  } else {
+    return null;
+  }
+}; 
 
 console.log(isFive(5));      // => 'is five'
 console.log(isFive(13));     // => null
